434ceec8eab830a337d612180896f7846dc5a071
acai.bjca <amalia.alfonso@mail.escuelaing.edu.co>
2019-08-16 12:48:37 -0500

    Corrigiendo error en clase SparkwebApp

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -10,6 +10,7 @@ import static spark.Spark.*;
 public class SparkWebApp {
 
     public static void main(String[] args) {
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
     }
 
